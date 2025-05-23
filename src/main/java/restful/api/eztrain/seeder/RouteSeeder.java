631b8c28001cde21fd052ed49df5e30d87ffb038
bintangginanjar <bintang.ginanjar@gmail.com>
2025-05-23 15:25:08 +0700

    feat: Add routes seeder & modifying user role seeder

---
+++ src/main/java/restful/api/eztrain/seeder/RouteSeeder.java
@@ -0,0 +1,69 @@
+package restful.api.eztrain.seeder;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import restful.api.eztrain.entity.RouteEntity;
+import restful.api.eztrain.entity.StationEntity;
+import restful.api.eztrain.entity.UserEntity;
+import restful.api.eztrain.repository.RouteRepository;
+import restful.api.eztrain.repository.StationRepository;
+import restful.api.eztrain.repository.UserRepository;
+
+@Component
+public class RouteSeeder implements CommandLineRunner {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private StationRepository stationRepository;
+
+    @Autowired
+    private RouteRepository routeRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        UserEntity adminUser = userRepository.findByEmail("admin@gmail.com").orElse(null);
+
+        if (routeRepository.count() == 0) {
+            List<StationEntity> stations = stationRepository.findAll();
+
+            if (stations.size() < 10 || adminUser == null) {
+                System.out.println("Not enough data to seed routes (requires 10 stations + 1 user).");
+                return;
+            }
+
+            List<RouteEntity> routes = new ArrayList<>();
+
+            routes.add(createRoute(stations.get(0), stations.get(1), 180.0, 180, adminUser));
+            routes.add(createRoute(stations.get(1), stations.get(2), 120.0, 150, adminUser));
+            routes.add(createRoute(stations.get(2), stations.get(3), 250.0, 240, adminUser));
+            routes.add(createRoute(stations.get(3), stations.get(4), 170.0, 210, adminUser));
+            routes.add(createRoute(stations.get(4), stations.get(5), 200.0, 220, adminUser));
+            routes.add(createRoute(stations.get(5), stations.get(6), 90.0, 100, adminUser));
+            routes.add(createRoute(stations.get(6), stations.get(7), 100.0, 110, adminUser));
+            routes.add(createRoute(stations.get(7), stations.get(8), 130.0, 140, adminUser));
+            routes.add(createRoute(stations.get(8), stations.get(9), 160.0, 180, adminUser));
+            routes.add(createRoute(stations.get(9), stations.get(0), 300.0, 300, adminUser));
+
+            routeRepository.saveAll(routes);
+            System.out.println("10 routes seeded successfully.");
+        }
+    }
+
+    private RouteEntity createRoute(StationEntity origin, StationEntity destination, Double distance, Integer duration, UserEntity user) {
+        RouteEntity route = new RouteEntity();
+        route.setOrigin(origin);
+        route.setDestination(destination);
+        route.setTripDistance(distance);
+        route.setTripDuration(duration);
+        route.setUserEntity(user);
+
+        return route;
+    }
+}
---
+++ src/main/java/restful/api/eztrain/seeder/StationSeeder.java
@@ -0,0 +1,57 @@
+package restful.api.eztrain.seeder;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import restful.api.eztrain.entity.StationEntity;
+import restful.api.eztrain.entity.UserEntity;
+import restful.api.eztrain.repository.StationRepository;
+import restful.api.eztrain.repository.UserRepository;
+
+@Component
+public class StationSeeder implements CommandLineRunner {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private StationRepository stationRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        UserEntity adminUser = userRepository.findByEmail("admin@gmail.com").orElse(null);
+
+        if (stationRepository.count() == 0) {
+            List<StationEntity> stations = new ArrayList<>();
+            stations.add(createStation("GMR", "Gambir", "Jakarta", "DKI Jakarta", adminUser));
+            stations.add(createStation("BD", "Bandung", "Bandung", "Jawa Barat", adminUser));
+            stations.add(createStation("YK", "Yogyakarta", "Yogyakarta", "DI Yogyakarta", adminUser));
+            stations.add(createStation("SGU", "Surabaya Gubeng", "Surabaya", "Jawa Timur", adminUser));
+            stations.add(createStation("SMT", "Semarang Tawang", "Semarang", "Jawa Tengah", adminUser));
+            stations.add(createStation("ML", "Malang", "Malang", "Jawa Timur", adminUser));
+            stations.add(createStation("PDL", "Padalarang", "Bandung Barat", "Jawa Barat", adminUser));
+            stations.add(createStation("CMB", "Cirebon", "Cirebon", "Jawa Barat", adminUser));
+            stations.add(createStation("KTA", "Kutoarjo", "Purworejo", "Jawa Tengah", adminUser));
+            stations.add(createStation("SR", "Solo Balapan", "Surakarta", "Jawa Tengah", adminUser));
+            
+            stationRepository.saveAll(stations);
+            System.out.println("10 stations seeded successfully.");
+        }        
+    }
+
+    private StationEntity createStation(String code, String name, String city, String province, UserEntity user) {
+        StationEntity station = new StationEntity();
+        station.setCode(code);
+        station.setName(name);
+        station.setCity(city);
+        station.setProvince(province);
+        station.setIsActive(true);
+        station.setUserEntity(user);
+        return station;
+    }
+
+}
